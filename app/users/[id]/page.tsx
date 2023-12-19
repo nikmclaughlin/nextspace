3e7d9785d79b0e9e179e3462e08a25922a5ee0ce
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2023-12-18 21:33:19 -0800

    use FollowButton

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,32 +1,33 @@
-import { prisma } from "@/lib/prisma"
-import Image from "next/image"
+import FollowButton from '../../components/FollowButton/FollowButton'
+import { prisma } from '@/lib/prisma'
+import Image from 'next/image'
 
 interface Props {
-    params: {id: string}
+  params: { id: string }
 }
 
-export async function generateMetadata({ params }: Props){
-    const user = await prisma.user.findUnique({ where: {id: params.id} })
-    return {title: `User profile of ${user?.name}` }
+export async function generateMetadata({ params }: Props) {
+  const user = await prisma.user.findUnique({ where: { id: params.id } })
+  return { title: `User profile of ${user?.name}` }
 }
 
-export default async function UserProfile({ params }: Props){
+export default async function UserProfile({ params }: Props) {
+  const user = await prisma.user.findUnique({ where: { id: params.id } })
+  const { name, bio, image } = user ?? {}
 
-    const user = await prisma.user.findUnique({ where: {id: params.id} })
-    const { name, bio, image } = user ?? {}
+  return (
+    <main>
+      <h1>{name}</h1>
+      <Image
+        src={image ?? '/mememan.webp'}
+        width={300}
+        height={300}
+        alt={`${name}'s profile`}
+      />
+      <h3>Bio</h3>
+      <p>{bio}</p>
 
-
-    return (
-        <main>
-            <h1>{name}</h1>
-            <Image
-                src={image ?? '/mememan.webp'}
-                width={300}
-                height={300}
-                alt={`${name}'s profile`}
-            />
-            <h3>Bio</h3>
-            <p>{bio}</p>
-        </main>
-    )
+      <FollowButton targetUserId={params.id} />
+    </main>
+  )
 }
